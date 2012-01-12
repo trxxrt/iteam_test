2c03c834287bfd37d7dc3e882ca6b1085c43c444
Zaurak <zaurak@zaurak-laptop.(none)>
2012-01-12 15:57:44 +0100

    A dangerous program...

---
+++ doxyTEST.cpp
@@ -0,0 +1,22 @@
+//! Generate a stack overflow
+/*! \brief General Recommendations : Do not use.
+
+#include <iostream>
+
+
+//! Prototypes
+void recursive_call();
+
+//! Main function
+int main(int argc, char* argv[])
+{
+	recursive_call();
+	return 1;
+}
+
+//! Procedure for a recursive call
+void recursive_call()
+{
+	char dummy_tab[1000];
+	recursive_call();
+}
